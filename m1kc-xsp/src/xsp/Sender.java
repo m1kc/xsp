59a93ffa4c25e3c705f60fc6a1aecabb68d9c248
m1kc <m1kc@yandex.ru>
2011-02-10 14:01:00 +0000

    Added two methods for a new version.

--- m1kc-xsp/src/xsp/Sender.java
+++ m1kc-xsp/src/xsp/Sender.java
@@ -44,4 +44,37 @@ public class Sender implements XSPConstants
         }
         u.binarySent(type, body);
     }
+
+    /**
+     * @deprecated Not used yet.
+     */
+    public static void sendPack(DataOutputStream os, int type, String utf, byte[] bytes, UIProxy u)
+    {
+        sendPack(os, type, new String[]{utf}, bytes, u);
+    }
+
+    /**
+     * @deprecated Not used yet.
+     */
+    public static void sendPack(DataOutputStream os, int type, String[] utf, byte[] bytes, UIProxy u)
+    {
+        try {
+            os.writeInt(type);
+
+            if (utf==null) utf = new String[0];
+            os.writeInt(utf.length);
+            for (int i=0; i<utf.length; i++) os.writeUTF(utf[i]);
+
+            if (bytes==null) bytes = new byte[0];
+            os.writeInt(bytes.length);
+            os.write(bytes);
+
+            os.flush();
+        } catch (IOException ex) {
+            u.errorWhileSending(ex);
+            Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //u.binarySent(type, body);
+        // NOTIFY UIPROXY!!!
+    }
 }
