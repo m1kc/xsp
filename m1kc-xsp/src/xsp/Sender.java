b9c5ce51ee7bbc949e47509482adedc2c8bfa42e
m1kc <m1kc@yandex.ru>
2011-02-10 20:27:50 +0000

    Started to work at 2nd generation

--- m1kc-xsp/src/xsp/Sender.java
+++ m1kc-xsp/src/xsp/Sender.java
@@ -16,65 +16,58 @@ import java.util.logging.Logger;
 public class Sender implements XSPConstants
 {
 
+    /**
+     * @deprecated
+     */
     public static void sendUTF(DataOutputStream os, int type, String body, UIProxy u)
     {
-        try {
-            os.writeByte(TEXT);
-            os.writeInt(type);
-            if (body==null) os.writeUTF(""); else os.writeUTF(body);
-            os.flush();
-        } catch (IOException ex) {
-            u.errorWhileSending(ex);
-            Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        u.textSent(type, body);
+        sendPack(os, type, body, null, u);
     }
 
+    /**
+     * @deprecated
+     */
     public static void sendBytes(DataOutputStream os, int type, byte[] body, UIProxy u)
     {
-        try {
-            os.writeByte(BINARY);
-            os.writeInt(type);
-            os.writeInt(body.length);
-            if (body==null) os.write(new byte[0]); else os.write(body);
-            os.flush();
-        } catch (IOException ex) {
-            u.errorWhileSending(ex);
-            Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        u.binarySent(type, body);
+        sendPack(os, type, (String[])null, body, u);
     }
 
-    /**
-     * @deprecated Not used yet.
-     */
     public static void sendPack(DataOutputStream os, int type, String utf, byte[] bytes, UIProxy u)
     {
-        sendPack(os, type, new String[]{utf}, bytes, u);
+        if (utf == null) sendPack(os, type, (String[]) null, bytes, u);
+        else sendPack(os, type, new String[]{utf}, bytes, u);
     }
 
-    /**
-     * @deprecated Not used yet.
-     */
     public static void sendPack(DataOutputStream os, int type, String[] utf, byte[] bytes, UIProxy u)
     {
         try {
             os.writeInt(type);
 
-            if (utf==null) utf = new String[0];
-            os.writeInt(utf.length);
-            for (int i=0; i<utf.length; i++) os.writeUTF(utf[i]);
+            if (utf==null)
+            {
+                os.writeInt(0);
+            }
+            else
+            {
+                os.writeInt(utf.length);
+                for (int i=0; i<utf.length; i++) os.writeUTF(utf[i]);
+            }
 
-            if (bytes==null) bytes = new byte[0];
-            os.writeInt(bytes.length);
-            os.write(bytes);
+            if (bytes==null)
+            {
+                os.writeInt(0);
+            }
+            else
+            {
+                os.writeInt(bytes.length);
+                os.write(bytes);
+            }
 
             os.flush();
         } catch (IOException ex) {
             u.errorWhileSending(ex);
             Logger.getLogger(Sender.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //u.binarySent(type, body);
-        // NOTIFY UIPROXY!!!
+        u.packSent(type, utf, bytes);
     }
 }
