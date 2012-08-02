d665efc489c2256d50027e21337e06ef4daaae05
m1kc <m1kc@yandex.ru>
2012-08-03 01:33:53 +0400

    Improved LookAndFeel detection.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,6 +5,8 @@
 
 package ui;
 
+import javax.swing.UIManager;
+
 /**
  *
  * @author m1kc
@@ -20,31 +22,65 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         for (int i=0; i<args.length; i++)
         {
             if (args[i].hashCode()=="--developer".hashCode()) developer = true;
             if (args[i].hashCode()=="-d".hashCode()) developer = true;
         }
         
-        setLookAndFeel(new com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel());
+        setLookAndFeel();
         
         cf = new ConnectFrame();
         if (!developer) cf.jButton3.setVisible(false);
         cf.setVisible(true);
         od = new OpenDialog();
     }
-
-    public static void setLookAndFeel(javax.swing.LookAndFeel f)
+    
+    private static void setLookAndFeel()
     {
+        // Well, let's try to set Nimbus explicitly first.
         try
         {
-            if (f==null) javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
-            else javax.swing.UIManager.setLookAndFeel(f);
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            System.out.println("LaF: com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            return; // success
         }
-        catch (Throwable ex)
+        catch(Throwable ex)
         {
+            // do nothing
+        }
+        
+        // No? Let's try to find proper class name from the installed LookAndFeel's.
+        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+        {
+            if ("Nimbus".equals(info.getName()))
+            {
+                try
+                {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    System.out.println("LaF: "+info.getClassName()+" (found)");
+                    return; // success
+                }
+                catch (Throwable ex)
+                {
+                    // do nothing
+                }
+            }
+        }
+        
+        // Still no? Let's just use the system one.
+        try
+        {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            System.out.println("LaF: "+UIManager.getSystemLookAndFeelClassName()+" (system)");
+            return; // success
+        }
+        catch (Throwable ex2)
+        {
+            System.out.println("LaF: everything failed, leaving default one");
+            // Sadly.
         }
     }
-
 }
